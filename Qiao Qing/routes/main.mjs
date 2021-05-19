81647bdb0831d28b184e285b7c800f62b8d167a3
Voon Qiao Qing <77107489+VoonQiaoQing@users.noreply.github.com>
2021-05-20 01:52:34 +0800

    Add files via upload

--- Qiao Qing/routes/main.mjs
+++ Qiao Qing/routes/main.mjs
@@ -1,5 +1,6 @@
 import { Router }       from 'express';
 import { flashMessage } from '../utils/flashmsg.mjs'
+import { ModelHomeDescription } from '../data/homedescription.mjs';
 
 const router = Router();
 export default router;
@@ -15,11 +16,18 @@ router.get("/dynamic/:path", async function (req, res) {
 import RouterAuth from './auth.mjs'
 router.use("/auth", RouterAuth);
 
-
+router.get("/", home_page);
+router.post("/", home_process);
+/**
+ * Renders the login page
+ * @param {Request}  req Express Request handle
+ * @param {Response} res Express Response handle
+ */
 // ---------------- 
 //	TODO:	Common URL paths here
-router.get("/",      async function(req, res) {
+async function home_page(req, res) {
 	console.log("Home page accessed");
+		//	Create homedescrip
 	return res.render('home', {
 		homedescription: "Welcome to Golden TV, where we combine the two together to give you the most wonderous experience you seek within safe areas where you can get to enjoy your time with your loved ones!",
 		homepolicy: "Our No Smoking, No Frills policies are to promote healthy and green karaoke culture among",
@@ -28,6 +36,41 @@ router.get("/",      async function(req, res) {
 		release_name3: "3",
 		release_name4: "4"
 	});
+}
+/**
+ * Renders the login page
+ * @param {Request}  req Express Request handle
+ * @param {Response} res Express Response handle
+ */
+async function home_process(req, res) {
+	try {
+		const user = await ModelHomeDescription.create({
+			"email" : "req.body.email",
+			"homedescription": "homedescription"
+		});
+	console.log('Description created: $(user.uuid)');
+	}
+
+	catch (error) {
+		console.error(`Credentials problem: ${req.body.email} ${req.body.password}`);
+		console.error(error);
+		return res.render('auth/login', { errors: errors });
+	}
+}
+
+router.get("/edithomedes", async function(req, res) {
+	console.log("Home Description page accessed");
+	return res.render('edithomedescription');
+});
+
+router.get("/edithomeimagepolicy", async function(req, res) {
+	console.log("Home Policy page accessed");
+	return res.render('edithomeimagepolicy');
+});
+
+router.get("/editbestreleases", async function(req, res) {
+	console.log("Best Releases page accessed");
+	return res.render('editbestreleases');
 });
 
 router.get("/movies",      async function(req, res) {
@@ -48,6 +91,36 @@ router.get("/movies",      async function(req, res) {
 	});
 });
 
+router.get("/editrooms", async function(req, res) {
+	console.log("Rooms page accessed");
+	return res.render('editrooms');
+});
+
+router.get("/choosemoviesandsongs", async function(req, res) {
+	console.log("Choose Movies and Songs page accessed");
+	return res.render('choosemoviesandsongs');
+});
+
+router.get("/createmovies", async function(req, res) {
+	console.log("Create Movies page accessed");
+	return res.render('createmovies');
+});
+
+router.get("/updatemovies", async function(req, res) {
+	console.log("Update Movies page accessed");
+	return res.render('updatemovies');
+});
+
+router.get("/createsongs", async function(req, res) {
+	console.log("Create Songs page accessed");
+	return res.render('createsongs');
+});
+
+router.get("/updatesongs", async function(req, res) {
+	console.log("Update Songs page accessed");
+	return res.render('updatesongs');
+});
+
 router.get("/about", async function(req, res) {
 	console.log("About page accessed");
 	return res.render('about', {
@@ -55,6 +128,7 @@ router.get("/about", async function(req, res) {
 		values: [1, 2, 3, 4, 5, 6]
 	});
 });
+
 router.get('/about', (req, res) => {
 	const author = 'Denzel Washington';
 	let success_msg = 'Success message';
@@ -64,5 +138,4 @@ router.get('/about', (req, res) => {
 	success_msg: success_msg,
 	error_msg: error_msg
 	})
-	});
-	
+});
