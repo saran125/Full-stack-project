93749850a880d97b9b53094838ff29aa350c57a4
saran125 <kumarsaran522@gmail.com>
2021-07-07 12:08:01 +0800

    testing

--- FullStackProject/routes/faq.mjs
+++ FullStackProject/routes/faq.mjs
@@ -3,12 +3,10 @@ import {ModelFaq} from '../data/faq.mjs';
 import { flashMessage } from '../utils/flashmsg.mjs'
 const router = Router();
 export default router;
-// Hello nigel
 router.get("/faq",      async function(req, res) {
 	console.log("faq page accessed");
 	return res.render('createfaq');
 });
-
 router.post("/faq", async function (req, res) {
 	console.log("faq contents received");
 	console.log(req.body);
