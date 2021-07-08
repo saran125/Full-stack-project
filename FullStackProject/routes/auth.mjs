a17648abfa281e15baa70de0b261330fce1c026d
xlllxllx <77482429+xlllxllx@users.noreply.github.com>
2021-07-08 11:42:09 +0800

    Update auth.mjs

--- FullStackProject/routes/auth.mjs
+++ FullStackProject/routes/auth.mjs
@@ -1,19 +1,55 @@
 import { Router }       from 'express';
-import { flashMessage } from '../utils/flashmsg.mjs'
+import { flashMessage } from '../utils/flashmsg.mjs';
+import { ModelUser }    from '../data/user.mjs';
+import Hash             from 'hash.js';
+import session          from 'express-session';
+
 const router = Router();
 export default router;
 
-router.get("/login",      async function(req, res) {
+/**
+ * Regular expressions for form testing
+ **/ 
+const regexEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//	Min 3 character, must start with alphabet
+const regexName  = /^[a-zA-Z][a-zA-Z]{2,}$/;
+//	Min 8 character, 1 upper, 1 lower, 1 number, 1 symbol
+const regexPwd   = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+
+
+router.get("/login",    login_page);
+router.post("/login",   login_process);
+router.get("/register", register_page);
+router.post("/register", register_process);
+router.get("/profile", profile_page);
+
+/**
+ * Renders the login page
+ * @param {Request}  req Express Request handle
+ * @param {Response} res Express Response handle
+ */
+async function login_page(req, res) {
 	console.log("Login page accessed");
 	return res.render('auth/login');
-});
+}
 
-router.get("/register", async function(req, res) {
+/**
+ * Render the registration page
+ * @param {Request}  req Express Request handle
+ * @param {Response} res Express Response handle
+ */
+async function register_page(req, res) {
 	console.log("Register page accessed");
 	return res.render('auth/register');
-});
+}
 
-router.post("/login", async function (req, res) {
+/**
+ * Process the login form body
+ * @param {Request}  req Express Request handle
+ * @param {Response} res Express Response handle
+ */
+async function login_process(req, res) {
 	console.log("login contents received");
 	console.log(req.body);
 
@@ -21,46 +57,129 @@ router.post("/login", async function (req, res) {
 	//	Check your Form contents
 	//	Basic IF ELSE STUFF no excuse not to be able to do this alone
 	//	Common Sense
-	if (errors.length > 0) {
-		flashMessage(res, 'error', 'Invalid Credentials!', 'fas fa-sign-in-alt', true);
-		return res.redirect(req.originalUrl);
-	}
-	else {
-		flashMessage(res, 'success', 'Successfully login!', 'fas fa-sign-in-alt', true);
-		return res.redirect("/home");
-	}
-});
+	try {
+		if (! regexEmail.test(req.body.email)) {
+			errors = errors.concat({ text: "Invalid email address!" });
+		}
 
-router.post("/register", async function (req, res) {
+		if (! regexPwd.test(req.body.password)) {
+			errors = errors.concat({ text: "Password requires minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character!" });
+		}
+
+		if (errors.length > 0) {
+			throw new Error("There are validation errors");
+		}
+	}
+	catch (error) {
+		console.error("There is errors with the login form body.");
+		console.error(error);
+		return res.render('auth/login', { errors: errors });
+	}
+
+	try {
+		const user = await ModelUser.findOne({where: {
+			email: req.body.email,
+			password: Hash.sha256().update(req.body.password).digest("hex")
+		}});
+
+
+		if (user == null) {
+			errors = errors.concat({ text: "Invalid user credentials!" });
+		}
+
+		if (errors.length > 0) {
+			throw new Error("There are validation errors");
+		}
+		else {
+			flashMessage(res, 'success', 'Successfully login!', 'fas fa-sign-in-alt', true);
+			var loggin = req.session
+			session({
+				genid:function(req){
+					return genuuid()
+				},
+				secret:'random-secret'
+			});
+			return res.redirect("/home");
+		}
+	}
+	catch (error) {
+		console.error(`Credentials problem: ${req.body.email} ${req.body.password}`);
+		console.error(error);
+		return res.render('auth/login', { errors: errors });
+	}
+}
+
+/**
+ * Process the registration form body
+ * @param {Request}  req Express Request handle
+ * @param {Response} res Express Response handle
+ */
+async function register_process(req, res) {
 	console.log("Register contents received");
 	console.log(req.body);
 	let errors = [];
-
 	//	Check your Form contents
 	//	Basic IF ELSE STUFF no excuse not to be able to do this alone
 	//	Common Sense
+	try {
+		if (! regexName.test(req.body.name)) {
+			errors = errors.concat({ text: "Invalid name provided! It must be minimum 3 characters and starts with a alphabet." });
+		}
 
-	const regexEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	if (req.body.password != req.body.password2) {
-		errors = errors.concat({ text: "Password do not match !"});
+		if (! regexEmail.test(req.body.email)) {
+			errors = errors.concat({ text: "Invalid email address!" });
+		}
+		else {
+			const user = await ModelUser.findOne({where: {email: req.body.email}});
+			if (user != null) {
+				errors = errors.concat({ text: "This email cannot be used!" });
+			}
+		}
+
+		if (! regexPwd.test(req.body.password)) {
+			errors = errors.concat({ text: "Password requires minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one symbol!" });
+		}
+		else if (req.body.password !== req.body.password2) {
+			errors = errors.concat({ text: "Password do not match!" });
+		}
+
+		if (errors.length > 0) {
+			throw new Error("There are validation errors");
+		}
+	}
+	catch (error) {
+		console.error("There is errors with the registration form body.");
+		console.error(error);
+		return res.render('auth/register', { errors: errors });
 	}
 
-	if (!regexEmail.test(req.body.email)) {
-		errors = errors.concat({text: "Invalid Email address!"});
-	}
-
-	if (req.body.name == undefined || req.body.name.length < 4) {
-		errors = errors.concat({text: "Invalid Name"});
-	}
-
-	if (errors.length > 0) {
-		console.error(`There are ${errors.length} errors in the form`);
-		return res.render('auth/register', {
-			errors: errors
+	//	Create new user, now that all the test above passed
+	try {
+		const user = await ModelUser.create({
+				email:    req.body.email,
+				password: Hash.sha256().update(req.body.password).digest("hex"),
+				name:     req.body.name,
 		});
-	}
-	else {
+
 		flashMessage(res, 'success', 'Successfully created an account. Please login', 'fas fa-sign-in-alt', true);
 		return res.redirect("/auth/login");
 	}
-});
+	catch (error) {
+		//	Else internal server error
+		console.error(`Failed to create a new user: ${req.body.email} `);
+		console.error(error);
+		return res.status(500).end();
+	}
+};
+
+async function profile_page(req, res){
+	if (req.session){
+	console.log("Profile page accessed");
+	return res.render('auth/profile');
+	}
+	else{
+		console.log("Please login first.")
+		return res.render('auth/login');
+	}
+};
+	
